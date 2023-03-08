ad7e0dc00ca6e7417de6660c72a32bd8ae548157
ru39391 <dev@39391.ru>
2023-03-08 17:19:39 +0300

    support/настроить получение доп. параметров с данными услуг для ресурсов

--- Api/Page/GetController.php
+++ Api/Page/GetController.php
@@ -12,21 +12,42 @@ class GetController extends AuthController
 
   public function index()
   {
-    $resColl = $this->modx->getCollection(\modResource::class, array(
-      'deleted' => 0,
-      'published' => 1,
-      'template:IN' => array(8,9,15,16,17,18,19,20),
-      'parent:NOT IN' => array(0,3,5,37,40,230,243,356),
+    $query = $this->modx->newQuery(\modResource::class);
+    $query->select(array(
+      'modResource.id as id',
+      'modResource.pagetitle as pagetitle',
+      'modResource.parent as parent',
+      'modResource.publishedon as publishedon',
+      'modResource.editedon as editedon',
+      'modResource.isfolder as isfolder',
+      'modResource.template as template',
+      'modResource.uri as uri',
+      'modResource.deleted as deleted',
+      'modResource.published as published',
+      'department_id.value as dept_id',
+      'specialization_ids.value as subdept_id',
+      'group_ids.value as group_id'
     ));
+    $query->leftJoin(\modTemplateVarResource::class, 'department_id', 'modResource.id = department_id.contentid AND department_id.tmplvarid = 25');
+    $query->leftJoin(\modTemplateVarResource::class, 'specialization_ids', 'modResource.id = specialization_ids.contentid AND specialization_ids.tmplvarid = 26');
+    $query->leftJoin(\modTemplateVarResource::class, 'group_ids', 'modResource.id = group_ids.contentid AND group_ids.tmplvarid = 28');
+    $query->groupby('modResource.id');
+    $query->where(array(
+      'modResource.deleted' => 0,
+      'modResource.published' => 1,
+      'modResource.template:IN' => array(8,9,15,16,17,18,19,20),
+      'modResource.parent:NOT IN' => array(0,3,5,37,40,230,243,356),
+    ));
+
+    $resColl = $this->modx->getCollection(\modResource::class, $query);
     if (count($resColl) > 0) {
       $pages = array_map(fn($item) => array(
         'id' => $item->id,
         'pagetitle' => $item->pagetitle,
         'parent' => $item->parent,
-        //'department_id' => $this->getArr($item->getTVValue('department_id')),
-        //'specialization_ids' => $this->getArr($item->getTVValue('specialization_ids')),
-        //'group_ids' => $this->getArr($item->getTVValue('group_ids')),
-        //'show_pl' => (bool)$item->getTVValue('tv__show_pricelist'),
+        'dept' => $this->getArr($item->dept_id),
+        'subdept' => $this->getArr($item->subdept_id),
+        'group' => $this->getArr($item->group_id),
         'publishedon' => $item->publishedon,
         'editedon' => $item->editedon,
         'isfolder' => $item->isfolder,
