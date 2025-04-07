325a3ef38f1f31f07a336019ff5e96ee2468ecac
ru39391 <dev@39391.ru>
2025-04-07 21:46:16 +0300

    feature/реализовать вызов сниппета для оповещений при удалении услуг прайслиста

--- Api/Common/DeleteController.php
+++ Api/Common/DeleteController.php
@@ -10,12 +10,45 @@ class DeleteController extends CommonController
 {
   use CommonTrait;
 
+  private function handlePricelistItem($class, $data, $item)
+  {
+    $itemData = $item->toArray();
+    $output = $item->remove() ? $data : $itemData;
+
+    if($class !== \pricelistItem::class || $itemData[Constants::GROUP_KEY] > 0) {
+      return $output;
+    }
+
+    $resources = [];
+    $pricelistLinks = $this->modx->getCollection(\pricelistLink::class);
+
+    foreach ($pricelistLinks as $pricelistLinkItem) {
+      $linkItem = $pricelistLinkItem->toArray();
+      $config = json_decode($linkItem[Constants::CONFIG_KEY], true);
+      $pricelist = json_decode($linkItem[Constants::ITEMS_PARAM_KEY], true);
+
+      if($config['isComplexData'] === true && in_array($itemData[Constants::ID_KEY], $pricelist)) {
+        $resources[] = $linkItem[Constants::ID_KEY];
+      }
+    }
+
+    if(count($resources) > 0) {
+      zoomx('modx')->runSnippet('sendResLinksData', array(
+        'resources' => $resources,
+        'item' => $itemData
+      ));
+    }
+
+    return $output;
+  }
+
   private function deleteItem($data, $dateKey, $class)
   {
     $output = [];
     $item = $this->getItem($class, $data);
+
     if ((bool)$item) {
-      $output = $item->remove() ? $data : $item->toArray();
+      $output = $this->handlePricelistItem($class, $data, $item);
       zoomx('modx')->cacheManager->clearCache();
     } else {
       $output = $data;
