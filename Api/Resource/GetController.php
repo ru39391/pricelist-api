f02fc1f1df7a7cbfae21fc8472e5925f1a2df61d
ru39391 <dev@39391.ru>
2025-04-10 15:34:21 +0300

    feature/обновить параметры выборки ресурсов

--- Api/Resource/GetController.php
+++ Api/Resource/GetController.php
@@ -9,16 +9,29 @@ class GetController extends AuthController
   public function index()
   {
     $response = [];
+    $pages = [];
+    $features = [];
     $parents = [];
     $resources = [];
     $templates = [];
     $tplIds = [];
 
-    $props = array(
-      'deleted' => 0,
-      'published' => 1,
+    $props = array('deleted' => 0, 'published' => 1);
+    $pubResourcesList = $this->modx->getCollection(\modResource::class, $props);
+    /*
+    8 - ДЕТСКОЕ ОТДЕЛЕНИЕ;
+    40 - Отзывы;
+    168 - Рефлексотерапия;
+    230 - Хайлайты;
+    240 - Тестирование и разработка;
+    243 - Технические страницы;
+    356 - Примеры работ;
+    */
+    $unpubParentsList = $this->modx->getCollection(
+      \modResource::class,
+      array('id:IN' => [8,168,230,240])
     );
-    $resourcesList = $this->modx->getCollection(\modResource::class, $props);
+    $resourcesList = array_merge($pubResourcesList, $unpubParentsList);
 
     usort($resourcesList, fn($a, $b) => $a->id - $b->id);
 
@@ -26,9 +39,21 @@ class GetController extends AuthController
       foreach ($resourcesList as $res) {
         $data = $res->toArray();
 
-        $resources[] = $data;
         $parents[] = $res->parent;
         $tplIds[] = $res->template;
+
+        if(in_array($res->parent, [40,230,240,243,356])) {
+          $features[] = $data;
+        } else {
+          $pages[] = $data;
+        }
+      }
+
+      foreach ($pages as $key => $page) {
+        if(in_array($page['parent'], array_map(fn($data) => $data['id'], $features))) {
+          unset($pages[$key]);
+          $features = array_merge($features, array($page));
+        }
       }
 
       $templatesList = $this->modx->getCollection(
@@ -43,11 +68,14 @@ class GetController extends AuthController
       $response = array(
         'counter' => array(
           'parents' => count(array_unique($parents)),
-          'resources' => count($resources),
+          'pages' => count($pages),
+          'features' => count($features),
+          'resources' => count($pages) + count($features),
         ),
-        'templates' => $templates,
+        'pages' => $pages,
+        'features' => $features,
         'parents' => array_unique($parents),
-        'resources' => $resources,
+        'templates' => $templates,
         'success' => true
       );
     }
